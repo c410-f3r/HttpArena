96a3fc77ea3f76aaf47147ba84f49cad63d0df1d
Andreas Nägeli <andr.naegeli@gmail.com>
2026-03-13 17:21:56 +0100

    Switch to internal engine, opt out of h2/h3

--- frameworks/genhttp/Program.cs
+++ frameworks/genhttp/Program.cs
@@ -1,24 +1,13 @@
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
 
 using genhttp;
 
-using GenHTTP.Engine.Kestrel;
-
-var certPath = Environment.GetEnvironmentVariable("TLS_CERT") ?? "/certs/server.crt";
-var keyPath = Environment.GetEnvironmentVariable("TLS_KEY") ?? "/certs/server.key";
-var hasCert = File.Exists(certPath) && File.Exists(keyPath);
+using GenHTTP.Engine.Internal;
 
 var app = Project.Create();
 
 var host = Host.Create().Handler(app);
 
 host.Bind(IPAddress.Any, 8080);
-
-if (hasCert)
-{
-    var cert = X509Certificate2.CreateFromPemFile(certPath, keyPath);
-    host.Bind(IPAddress.Any, 8443, cert, enableQuic: true);
-}
 
 await host.RunAsync();
