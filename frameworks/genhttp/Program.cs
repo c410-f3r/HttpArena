e7655d3d47f7f926d74bc18fdbc9aabcb3079c58
Andreas Nägeli <andr.naegeli@gmail.com>
2026-03-29 09:05:23 +0200

    [C#] GenHTTP: Enable SSL and configure compression globally

--- frameworks/genhttp/Program.cs
+++ frameworks/genhttp/Program.cs
@@ -1,13 +1,25 @@
 using System.Net;
-
+using System.Security.Cryptography.X509Certificates;
 using genhttp;
 
 using GenHTTP.Engine.Internal;
+using GenHTTP.Modules.Practices;
+
+var certPath = Environment.GetEnvironmentVariable("TLS_CERT") ?? "/certs/server.crt";
+var keyPath = Environment.GetEnvironmentVariable("TLS_KEY") ?? "/certs/server.key";
+var hasCert = File.Exists(certPath) && File.Exists(keyPath);
 
 var app = Project.Create();
 
-var host = Host.Create().Handler(app);
+var host = Host.Create()
+               .Handler(app)
+               .Defaults(clientCaching: false);
 
 host.Bind(IPAddress.Any, 8080);
+
+if (hasCert)
+{
+    host.Bind(IPAddress.Any, 8443, X509Certificate2.CreateFromPemFile(certPath, keyPath));
+}
 
 await host.RunAsync();
