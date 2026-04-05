ac6094ab780df8ad4847f9b23ff77faeacd6f8da
Andreas Svanberg <andreas.svanberg@mensa.se>
2026-04-05 12:04:34 +0200

    Test profile "sync-db"

--- frameworks/spring-boot/src/main/java/com/httparena/spring/boot/Application.java
+++ frameworks/spring-boot/src/main/java/com/httparena/spring/boot/Application.java
@@ -1,7 +1,13 @@
 package com.httparena.spring.boot;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.sqlite.SQLiteDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +16,18 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Qualifier("sqlite")
+	@Bean
+	public DataSource sqliteDataSource() {
+		SQLiteDataSource sqLiteDataSource = new SQLiteDataSource();
+		sqLiteDataSource.setUrl("jdbc:sqlite:/data/benchmark.db");
+		sqLiteDataSource.setReadOnly(true);
+		return sqLiteDataSource;
+	}
+
+	@Bean
+	@Qualifier("sqlite")
+	public JdbcClient sqliteJdbcClient(@Qualifier("sqlite") DataSource dataSource) {
+		return JdbcClient.create(dataSource);
+	}
 }
